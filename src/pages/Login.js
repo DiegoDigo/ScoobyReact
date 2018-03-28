4ba50ee388a35517101a231eaa2d64d2bdf17966
Diego <di3g0d0ming05@gmail.com>
2018-03-28 15:10:01 -0300

    criaçao da tela de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import M from 'materialize-css';
 import './css/Login.css';
 
 export default class Login extends Component {
@@ -10,18 +11,36 @@ export default class Login extends Component {
         }
     }
 
+      componentDidMount(){
+
+    }
+
     render() {
         return(
             <div className='form-login'>
-                <h1>Login</h1>
-                <form>
-                    <div class="input-field col s12">
-                        <i class="material-icons prefix">textsms</i>
-                        <input type="text" id="autocomplete-input" class="autocomplete" />
-                        <label for="autocomplete-input">Autocomplete</label>
-                    </div>
-                </form>
-            </div>
+                 <div class="row">
+                     <h1 className="center">Login Scooy</h1>
+                    <form class="col s12">
+                       <div class="row">
+                            <div class="input-field col s12">
+                                <input id="email" type="email" class="validate" />
+                                <label for="email">Email</label>
+                            </div>
+                          </div>
+                          <div class="row">
+                            <div class="input-field col s12">
+                              <input id="password" type="password" class="validate"/>
+                              <label for="password">Senha</label>
+                            </div>
+                          </div>
+                          <div class="row">
+                              <div class="input-field col s12">
+                              <input type="submit" value="Acessar" className="btn btn-block col s12 teal"/>
+                              </div>
+                          </div>
+                    </form>
+                  </div>
+                </div>
         )
     }
 }
